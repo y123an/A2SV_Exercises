91ecbe70039f12826384475f19eea1ff895455f1
EBISHU <yohannesaregahabte@gmail.com>
2023-04-30 13:29:40 +0300

    numbers of good pairs

---
+++ Number_of_good_pairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int count=0;
+        int n= nums.size();
+
+        for(int i = 0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]==nums[j] && i<j){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
