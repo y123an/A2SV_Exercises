72381a4df3b703f75d37dfdd76ec7877d7d542ec
y123an <yohannnsearegahabete@gmail.com>
2023-07-05 19:28:29 +0300

    move zeros

---
+++ move_zeros.py
@@ -0,0 +1,13 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l = 0
+        r = 0
+
+        while r < len(nums):
+            if nums[r] != 0:
+                nums[l],nums[r] = nums[r], nums[l]
+                l +=1
+            r +=1
