a432f1c1a6e7bba3e6357a562f641abe055979c5
y123an <yohannnsearegahabete@gmail.com>
2023-07-31 16:21:23 +0300

    2381. Shifting Letters II

---
+++ range-sum-query-immutable.py
@@ -0,0 +1,20 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.prefix = [0]
+
+        for num in nums:
+            self.prefix.append(self.prefix[-1] + num)
+        
+
+    def sumRange(self, left: int, right: int) -> int:
+        lefts = self.prefix[left]
+        rights = self.prefix[right + 1]
+        
+        return rights - lefts
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
